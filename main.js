d815f78c0dcf61928fabe4b6805195847bec87b4
eyesun <eyesunbidari@gmail.com>
2024-01-28 21:33:39 +0330

    create todo on Dom

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const todos = [];
 const todoInput = document.querySelector(".todo-input");
 const todoForm = document.querySelector(".todo-form");
+const todoList = document.querySelector(".todolist");
 
 todoForm.addEventListener("submit", addNewTodo);
 
@@ -12,8 +13,21 @@ function addNewTodo(e) {
   const newTodo = {
     id: Date.now(),
     createdAt: new Date().toISOString(),
-    title: todoInput.value,
+    title: todoInput.value, 
     isCompleted: false,
   };
   todos.push(newTodo);
+
+  // creat todos in Dom
+  let result = "";
+  todos.forEach((todo) =>{
+    result +=`<li class="todo">
+    <p class="todo__title">${todo.title}</p>
+    <span class="todo__createdAt">${new Date(todo.createdAt).toLocaleDateString("fa-IR")}</span>
+    <button data-todo-id=${todo.id}><i class="todo__check far fa-check-square"></i></button>
+    <button data-todo-id=${todo.id}><i class="todo__remove far fa-trash-alt"></i></button>
+  </li>`
+  });
+  todoList.innerHTML = result;
+  todoInput.value = "";
 }
